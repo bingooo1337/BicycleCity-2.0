0f949bbdecee93b328c8f853b5a39dd7feb4b3de
Volodymyr Kamyshanov <volodymyr.kamyshanov@simplex.ink>
2018-02-07 01:18:11 +0200

    create registration activity

--- app/src/main/java/com/diploma/volodymyr/bicyclecity/LoginActivity.kt
+++ app/src/main/java/com/diploma/volodymyr/bicyclecity/LoginActivity.kt
@@ -1,12 +1,18 @@
 package com.diploma.volodymyr.bicyclecity
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        registrationButton.setOnClickListener {
+            startActivity(Intent(this, RegistrationActivity::class.java))
+        }
     }
 }
