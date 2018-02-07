c93f340cfad42784877c65d999676e5fcc3d9d39
Volodymyr Kamyshanov <volodymyr.kamyshanov@simplex.ink>
2018-02-07 02:01:21 +0200

    add registration and login

--- app/src/main/java/com/diploma/volodymyr/bicyclecity/LoginActivity.kt
+++ app/src/main/java/com/diploma/volodymyr/bicyclecity/LoginActivity.kt
@@ -3,16 +3,52 @@ package com.diploma.volodymyr.bicyclecity
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_login.*
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
+    companion object {
+        val TAG = "LoginActivity"
+    }
+
+    private var firebaseAuth: FirebaseAuth? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+        firebaseAuth = FirebaseAuth.getInstance();
+
         registrationButton.setOnClickListener {
             startActivity(Intent(this, RegistrationActivity::class.java))
         }
+
+        loginButton.setOnClickListener {
+            if (emailEditText.text.isNotEmpty() && passwordEditText.text.isNotEmpty())
+                loginUser(emailEditText.text.toString(), passwordEditText.text.toString())
+            else
+                Toast.makeText(this, "Enter email & password", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val currentUser = firebaseAuth?.currentUser
+    }
+
+    private fun loginUser(email: String, password: String) {
+        firebaseAuth?.signInWithEmailAndPassword(email, password)
+                ?.addOnCompleteListener {
+                    if (it.isSuccessful) {
+                        Toast.makeText(this, "Login successfully", Toast.LENGTH_LONG).show()
+                        val currentUser = firebaseAuth?.currentUser
+                        Log.i(TAG, currentUser?.email)
+                    } else {
+                        Toast.makeText(this, "Wrong email or password", Toast.LENGTH_SHORT).show()
+                        Log.w(TAG, it.exception)
+                    }
+                }
     }
 }
--- app/src/main/java/com/diploma/volodymyr/bicyclecity/RegistrationActivity.kt
+++ app/src/main/java/com/diploma/volodymyr/bicyclecity/RegistrationActivity.kt
@@ -2,11 +2,40 @@ package com.diploma.volodymyr.bicyclecity
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_registration.*
+import com.google.firebase.auth.FirebaseAuth
+
 
 class RegistrationActivity : AppCompatActivity() {
+
+    private var firebaseAuth: FirebaseAuth? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registration)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        registerButton.setOnClickListener {
+            if (emailEditText.text.isNotEmpty() && passwordEditText.text.isNotEmpty())
+                createUser(emailEditText.text.toString(), passwordEditText.text.toString())
+            else
+                Toast.makeText(this, "Enter email & password", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun createUser(email: String, password: String) {
+        firebaseAuth?.createUserWithEmailAndPassword(email, password)
+                ?.addOnCompleteListener {
+                    if (it.isSuccessful) {
+                        Toast.makeText(this, "You are successfully registered!",
+                                Toast.LENGTH_SHORT).show()
+                        finish()
+                    } else {
+                        Toast.makeText(this, "Authentication failed!",
+                                Toast.LENGTH_SHORT).show()
+                    }
+                }
     }
 }
